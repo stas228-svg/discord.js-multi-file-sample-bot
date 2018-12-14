38e7c322587c2ad544f8d7af0957ed664f5b3126
ranakhas <33785157+ranakhas@users.noreply.github.com>
2018-12-14 10:22:55 +0500

    Help command

---
+++ commands/help.js
@@ -0,0 +1,6 @@
+exports.run = (client, message, args) => {
+message.channel.send({embed: {
+  color: 3447003,
+  description: "List of Commands can be found here: https://github com/VizuDev/Sunqual/"
+}});
+}
